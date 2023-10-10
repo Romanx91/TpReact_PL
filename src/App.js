a80fa3df00d154ca40e66aa3169e5bd6211532bb
Romanx91 <romanpy7@gmail.com>
2023-10-10 17:42:54 -0300

    login/register

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Login from "./components/login/Login";
+import Register from "./components/register/Register";
+import "./App.css";
+import { useRef, useState } from "react";
 
 function App() {
+  const [showRegister, setShowRegister] = useState(false);
+
+  const handleRegisterClick = () => {
+    setShowRegister(true);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Login onRegisterClick={handleRegisterClick} />
+      {showRegister && (
+        <div className="register-section">
+          <Register />
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/components/login/Login.js
@@ -0,0 +1,82 @@
+/* eslint-disable import/first */
+import Form from "react-bootstrap/Form";
+import { useRef, useState } from "react";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Login.css";
+
+const Login = ({ onLoggedIn, onRegisterClick }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const handleSignupClick = () => {
+    onRegisterClick();
+  };
+
+  const emailChangeHandler = (event) => {
+    if (emailRef.current.value.length > 0) {
+      emailRef.current.style.borderColor = "";
+      emailRef.current.style.outline = "";
+    }
+    setEmail(event.target.value);
+  };
+
+  const passwordChangeHandler = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const signInHandler = () => {
+    if (emailRef.current.value.length === 0) {
+      emailRef.current.focus();
+      emailRef.current.style.borderColor = "red";
+      emailRef.current.style.outline = "none";
+      alert("Email vacío!");
+      return;
+    }
+
+    if (password.length === 0) {
+      passwordRef.current.focus();
+      passwordRef.current.style.borderColor = "red";
+      passwordRef.current.style.outline = "none";
+      alert("Password vacío");
+      return;
+    }
+    onLoggedIn();
+    /* navigate("/home");*/
+  };
+
+  return (
+    <div className="container_FormLogin">
+      <div className="interior">
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+          <span> our </span>
+          <Button variant="primary" onClick={handleSignupClick}>
+            signup
+          </Button>
+        </Form>
+      </div>
+    </div>
+  );
+};
+export default Login;
